f118039678a288535a06e0274a32cb363d5304eb
mzfdt <118974124+mzfdt@users.noreply.github.com>
2023-11-20 13:22:54 +0800

    Create adb.py

---
+++ adb.py
@@ -0,0 +1,18 @@
+favorite_language='python '
+favorite_language
+favorite_language.rstrip()
+favorite_language
+#'python'
+#'python '
+#'python'
+--------------------------
+favorite_language='python '
+favorite_language=favorite_language.rstrip()
+favorite_language
+#输出为：'python'
+--------------------------
+favorite_language=' python '
+favorite_language.rstrip()
+favorite_language.lstrip()
+favorite_language.strip()
+#输出为：' python'   'python '  'python'
